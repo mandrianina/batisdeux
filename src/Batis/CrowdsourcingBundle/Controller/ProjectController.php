84bf089e9e3ce4208156e9f35c90ed1acaf5e871
mandrianina <mrazaf98@gmail.com>
2016-08-25 15:35:41 +0300

    ajout de index.html

--- src/Batis/CrowdsourcingBundle/Controller/ProjectController.php
+++ src/Batis/CrowdsourcingBundle/Controller/ProjectController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProjectController extends Controller
 {
+	public function indexAction()
+	{
+		return $this->render('BatisCrowdsourcingBundle:Project:index.html.twig');
+	}
+	
     public function addAction()
     {
         return $this->render('BatisCrowdsourcingBundle:Project:post-job.html.twig');
